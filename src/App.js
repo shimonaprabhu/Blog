df1191bfa34ca27c7cefbc13ea26a1e7664feb30
shimonaprabhu <0718shimona@gmail.com>
2021-02-23 14:13:30 +0530

    State Setting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React,{Component} from 'react';
 
 class App extends Component{
+    
+    constructor(){
+        super();
+        this.state = {displayBio: false};
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+
+    toggleDisplayBio(){
+        this.setState({displayBio: !this.state.displayBio});
+    }
+
     render(){
+        
+        
         return (
             <div>
                 <h1>Hello!</h1>
                 <p>My name is Shimona.</p>
                 <p>I'm an Engineer.</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in Bangalore</p>
+                            <button onClick = {this.toggleDisplayBio}>Show Less</button>
+                        </div>
+
+                        
+                    ) : (
+                        <div>
+                            <button onClick = {this.toggleDisplayBio}>Read More</button>
+                        </div>
+                    )
+                }
             </div>
         )
     }
